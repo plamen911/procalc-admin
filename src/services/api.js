0a153b9831638220b8d5b64afe57a15b6e7e2ec4
Plamen Markov <plamen@lynxlake.org>
2025-06-19 23:48:02 +0300

    Changed to prod. API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://127.0.0.1:8000/'; // Base API URL
-//const API_URL = 'https://propcalc.zastrahovaite.com/'; // Base API URL
+// const API_URL = 'https://127.0.0.1:8000/'; // Base API URL
+const API_URL = 'https://propcalc.zastrahovaite.com/'; // Base API URL
 
 // Local storage key for JWT token
 const TOKEN_KEY = 'admin_jwt_token';
